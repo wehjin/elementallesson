b3bc56286ba57b643cb8e8dfb2c9ff2756a08ad5
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-02-21 08:34:33 -0800

    Add chapter 2 vocab. Add ripple for quiz titles.

--- app/src/main/java/com/rubyhuntersky/quizmaker/QuizTitleViewHolder.kt
+++ app/src/main/java/com/rubyhuntersky/quizmaker/QuizTitleViewHolder.kt
@@ -9,11 +9,9 @@ import kotlinx.android.synthetic.main.view_quiz_title.view.*
 class QuizTitleViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(createView(parent)) {
 
     fun bind(title: String, onClick: () -> Unit) {
-        with(itemView.titleTextView) {
-            text = title
-            setOnClickListener {
-                onClick()
-            }
+        itemView.titleTextView.text = title
+        itemView.quizTitleLayout.setOnClickListener {
+            onClick()
         }
     }
 
