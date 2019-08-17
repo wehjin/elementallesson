8bdc162b15d8a709228c3cf1ae0433053383f0cd
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-08-17 11:42:32 -0700

    Add Chap2.

--- data/src/main/kotlin/com/rubyhuntersky/data/material/Sem1CourseMaterial.kt
+++ data/src/main/kotlin/com/rubyhuntersky/data/material/Sem1CourseMaterial.kt
@@ -5,6 +5,7 @@ import com.rubyhuntersky.data.material.core.CourseMaterial
 object Sem1CourseMaterial : CourseMaterial {
     override val title = "Semester 1"
     override val subtitle = "Chapters 1-10"
-    override val lessons =
-        Sem1Chap01CourseMaterial.lessons + Sem1Chap10CourseMaterial.lessons
+    override val lessons = Sem1Chap01CourseMaterial.lessons +
+            Sem1Chap02CourseMaterial.lessons +
+            Sem1Chap10CourseMaterial.lessons
 }
