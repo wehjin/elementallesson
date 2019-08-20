31368b923f6f6d72085660d2fd69d0d6f420a93b
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-08-20 08:20:52 -0700

    Add ch1 vocab.

--- data/src/main/kotlin/com/rubyhuntersky/data/material/BasicDegreeMaterial.kt
+++ data/src/main/kotlin/com/rubyhuntersky/data/material/BasicDegreeMaterial.kt
@@ -1,11 +1,7 @@
 package com.rubyhuntersky.data.material
 
-import com.rubyhuntersky.data.material.core.CourseMaterial
 import com.rubyhuntersky.data.material.core.DegreeMaterial
 
 object BasicDegreeMaterial : DegreeMaterial {
-    override val courses: List<CourseMaterial> = listOf(
-        Sem1CourseMaterial,
-        Sem1Chap10CourseMaterial
-    )
+    override val courses = listOf(Sem1CourseMaterial, Sem2CourseMaterial)
 }
