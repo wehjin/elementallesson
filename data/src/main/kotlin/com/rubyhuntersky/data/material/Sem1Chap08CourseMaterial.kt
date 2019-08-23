c99e3a016adfc719e7b858c231d1ac65c8183581
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-08-23 09:28:39 -0700

    Add ch8.

---
+++ data/src/main/kotlin/com/rubyhuntersky/data/material/Sem1Chap08CourseMaterial.kt
@@ -0,0 +1,55 @@
+package com.rubyhuntersky.data.material
+
+import com.rubyhuntersky.data.material.core.CourseMaterial
+import com.rubyhuntersky.data.material.core.LessonMaterial
+
+object Sem1Chap08CourseMaterial : CourseMaterial {
+    override val title = "Chapter 8"
+    override val subtitle = "Semester 1"
+    override val lessons = listOf(
+        LessonMaterial(level = 8, prompt = "part-time job; side job", response = "アルバイト"),
+        LessonMaterial(level = 8, prompt = "driving", promptColor = "noun\nvehicles", response = "うんてん"),
+        LessonMaterial(level = 8, prompt = "sweets; candy", response = "おかし"),
+        LessonMaterial(level = 8, prompt = "customer", response = "おきゃくさま"),
+        LessonMaterial(level = 8, prompt = "section chief", response = "かちょう"),
+        LessonMaterial(level = 8, prompt = "golf", response = "ゴルフ"),
+        LessonMaterial(level = 8, prompt = "judo", response = "じゅうどう"),
+        LessonMaterial(level = 8, prompt = "skateboard", response = "スケートボード"),
+        LessonMaterial(level = 8, prompt = "sales", response = "セールス"),
+        LessonMaterial(level = 8, prompt = "tatami mat", response = "たたみ"),
+        LessonMaterial(level = 8, prompt = "chess", response = "チェス"),
+        LessonMaterial(level = 8, prompt = "tea ceremony", response = "(お)ちゃかい"),
+        LessonMaterial(level = 8, prompt = "when; at the time of", response = "とき"),
+        LessonMaterial(level = 8, prompt = "~minutes", response = "〜ふん", responseColor = "〜ぷん: 1, 3, 4, 6, 8?, 10, なん"),
+        LessonMaterial(level = 8, prompt = "bowling", response = "ボールング"),
+        LessonMaterial(level = 8, prompt = "interview", promptColor = "noun, japanese", response = "めんせつ"),
+        LessonMaterial(level = 8, prompt = "take; accept", promptColor = "e.g. an exam", response = "うける", responseColor = "ru verb"),
+        LessonMaterial(level = 8, prompt = "borrow", response = "かりる", responseColor = "ru verb"),
+        LessonMaterial(level = 8, prompt = "answer", promptColor = "verb", response = "こたえる", responseColor = "ru verb"),
+        LessonMaterial(level = 8, prompt = "be able to; can", response = "できる", responseColor = "ru verb"),
+        LessonMaterial(level = 8, prompt = "sit", response = "すわる", responseColor = "u verb\nmotion, uses に particle"),
+        LessonMaterial(level = 8, prompt = "get on; ride", response = "のる", responseColor = "u verb\nmotion, uses に particle"),
+        LessonMaterial(level = 8, prompt = "do a part-time job", response = "アルバイト（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 8, prompt = "drive", promptColor = "verb", response = "うんてん（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 8, prompt = "ask a question", response = "しつもん（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 8, prompt = "worry", promptColor = "verb", response = "しんぱい（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 8, prompt = "talk about something", response = "はなし（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 8, prompt = "all right; OK; fine", promptColor = "adjective", response = "だいじょうぶ", responseColor = "na adjective"),
+        LessonMaterial(level = 8, prompt = "sweet", promptColor = "adjective", response = "あまい", responseColor = "i adjective"),
+        LessonMaterial(level = 8, prompt = "bitter", promptColor = "adjective", response = "にがい", responseColor = "i adjective"),
+        LessonMaterial(level = 8, prompt = "until now", response = "いままで"),
+        LessonMaterial(level = 8, prompt = "probably", response = "たぶん"),
+        LessonMaterial(level = 8, prompt = "not especially; nothing in particular", response = "べつに"),
+        LessonMaterial(level = 8, prompt = "again", response = "また"),
+        LessonMaterial(level = 8, prompt = "I wonder", promptColor = "tail particle", response = "かな"),
+        LessonMaterial(level = 8, prompt = "with", promptColor = "particle marking audience or partner", response = "と"),
+        LessonMaterial(level = 8, prompt = "You can perform a tea ceremony!", response = "おちゃができるの。"),
+        LessonMaterial(level = 8, prompt = "I am fine with cars, too.", response = "くるまもだいじょうぶです。"),
+        LessonMaterial(level = 8, prompt = "Take the exam, please.", response = "しけんをうけてください。"),
+        LessonMaterial(level = 8, prompt = "I am going to do something rude.",promptColor = "I am entering the room.", response = "しつれいします。"),
+        LessonMaterial(level = 8, prompt = "Please don't worry.", response = "しんぱいしないで。"),
+        LessonMaterial(level = 8, prompt = "Perfect timing.", response = "ちょうどよかった。"),
+        LessonMaterial(level = 8, prompt = "Please come in.", response = "どうぞおはいりください。"),
+        LessonMaterial(level = 8, prompt = "I am Akemi Yamamoto.", response = "やまもとあけみともうします。")
+    )
+}
