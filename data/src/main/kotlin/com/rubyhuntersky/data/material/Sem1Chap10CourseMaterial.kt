284e03c7873c7cf6224f0683302761e0cfe9827c
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-08-25 10:37:53 -0700

    Add ch10 kanji.

--- data/src/main/kotlin/com/rubyhuntersky/data/material/Sem1Chap10CourseMaterial.kt
+++ data/src/main/kotlin/com/rubyhuntersky/data/material/Sem1Chap10CourseMaterial.kt
@@ -7,26 +7,11 @@ object Sem1Chap10CourseMaterial : CourseMaterial {
     override val title = "Chapter 10"
     override val subtitle = "Semester 1"
     override val lessons = listOf(
-        LessonMaterial(
-            level = 10,
-            prompt = "someone else's house",
-            response = "おたく",
-            responseColor = "May I escort you home?\nおたくまでおくりましょうか。"
-        ),
+        LessonMaterial(level = 10, prompt = "someone else's house", response = "おたく", responseColor = "May I escort you home?\nおたくまでおくりましょうか。"),
         LessonMaterial(level = 10, prompt = "sound", response = "おと"),
-        LessonMaterial(
-            level = 10,
-            prompt = "adult",
-            response = "大人",
-            responseColor = "おとな"
-        ),
+        LessonMaterial(level = 10, prompt = "adult", response = "大人", responseColor = "おとな"),
         LessonMaterial(level = 10, prompt = "online", response = "オンライン"),
-        LessonMaterial(
-            level = 10,
-            prompt = "semester",
-            response = "学き",
-            responseColor = "がっき"
-        ),
+        LessonMaterial(level = 10, prompt = "semester", response = "学き", responseColor = "がっき"),
         LessonMaterial(level = 10, prompt = "chewing gum", response = "ガム"),
         LessonMaterial(level = 10, prompt = "cell phone", response = "けいたい"),
         LessonMaterial(level = 10, prompt = "textbook", response = "きょうかしょ"),
@@ -35,197 +20,155 @@ object Sem1Chap10CourseMaterial : CourseMaterial {
         LessonMaterial(level = 10, prompt = "syllabus", response = "シラバス"),
         LessonMaterial(level = 10, prompt = "sports car", response = "スポーツカー"),
         LessonMaterial(level = 10, prompt = "slippers", response = "スリッパ"),
-        LessonMaterial(
-            level = 10,
-            prompt = "last semester",
-            response = "先学き",
-            responseColor = "せんがっき"
-        ),
+        LessonMaterial(level = 10, prompt = "last semester", response = "先学き", responseColor = "せんがっき"),
         LessonMaterial(level = 10, prompt = "hand; arm", response = "て"),
-        LessonMaterial(
-            level = 10,
-            prompt = "something",
-            response = "何か",
-            responseColor = "なにか"
-        ),
+        LessonMaterial(level = 10, prompt = "something", response = "何か", responseColor = "なにか"),
         LessonMaterial(level = 10, prompt = "number", response = "ばんごう"),
         LessonMaterial(level = 10, prompt = "secret", response = "ひみつ"),
         LessonMaterial(level = 10, prompt = "present; gift", response = "プレゼント"),
         LessonMaterial(level = 10, prompt = "bath", response = "(お)ふろ"),
         LessonMaterial(level = 10, prompt = "boss", response = "ボス"),
         LessonMaterial(level = 10, prompt = "window", response = "まど"),
+        LessonMaterial(level = 10, prompt = "open", response = "あける", responseColor = "ru verb"),
+        LessonMaterial(level = 10, prompt = "be late for; be delayed", response = "おくれる", responseColor = "ru verb, に　particle\nWhy were you late to the party?\nどうして パーティーに おくれましたか。"),
+        LessonMaterial(level = 10, prompt = "tell", response = "おしえる", responseColor = "ru verb"),
+        LessonMaterial(level = 10, prompt = "close; shut", response = "しめる", responseColor = "ru verb"),
+        LessonMaterial(level = 10, prompt = "inform", response = "知らせる", responseColor = "ru verb\nしらせる"),
+        LessonMaterial(level = 10, prompt = "turn on", response = "つける", responseColor = "ru verb"),
+        LessonMaterial(level = 10, prompt = "forget", response = "わすれる", responseColor = "ru verb"),
+        LessonMaterial(level = 10, prompt = "say", response = "言う", responseColor = "u verb\nいう"),
+        LessonMaterial(level = 10, prompt = "finish; end", response = "おわる", responseColor = "u verb, が particle\nThe exam ended.\nしけんが　おわりました。"),
+        LessonMaterial(level = 10, prompt = "chew", response = "かむ", responseColor = "u verb"),
+        LessonMaterial(level = 10, prompt = "do one's best", response = "がんばる", responseColor = "u verb"),
+        LessonMaterial(level = 10, prompt = "turn off; erase", response = "けす", responseColor = "u verb"),
+        LessonMaterial(level = 10, prompt = "smoke; inhale", response = "すう", responseColor = "u verb"),
+        LessonMaterial(level = 10, prompt = "use", response = "つかう", responseColor = "u verb"),
+        LessonMaterial(level = 10, prompt = "take", response = "とる", responseColor = "u verb"),
+        LessonMaterial(level = 10, prompt = "begin", response = "はじまる", responseColor = "u verb, が particle\nSummer vacation will begin.\nなつ休みが　はじまります。"),
+        LessonMaterial(level = 10, prompt = "be absent from", response = "休む", responseColor = "u verb, を particle\nやすむ\nYou cannot skip class.\nクラスを　休んでは　いけませんよ。"),
+        LessonMaterial(level = 10, prompt = "explain", response = "せつめい（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 10, prompt = "consult", response = "そうだん（を）する", responseColor = "irregular verb, に particle\nWho shall we consult with?\nだれに そうだんしましょうか。"),
+        LessonMaterial(level = 10, prompt = "practice", response = "れんしゅう（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 10, prompt = "contact", promptColor = "verb", response = "れんらく（を）する", responseColor = "irregular verb"),
+        LessonMaterial(level = 10, prompt = "important", response = "大せつ", responseColor = "na adjective\nたいせつ"),
+        LessonMaterial(level = 10, prompt = "hard", response = "大へん", responseColor = "na adjective\nたいへん"),
+        LessonMaterial(level = 10, prompt = "no good; bad", response = "だめ", responseColor = "na adjective"),
+        LessonMaterial(level = 10, prompt = "always", response = "いつも"),
+        LessonMaterial(level = 10, prompt = "without fail; surely", response = "かならず"),
+        LessonMaterial(level = 10, prompt = "properly", response = "ちゃんと"),
+        LessonMaterial(level = 10, prompt = "yet", response = "まだ"),
+        LessonMaterial(level = 10, prompt = "already", response = "もう"),
+        LessonMaterial(level = 10, prompt = "okay; OK; alright", response = "オーケー"),
+        LessonMaterial(level = 10, prompt = "Let's study hard.", response = "がんばってべんきょうしましょう。"),
+        LessonMaterial(level = 10, prompt = "under any circumstance; definitely", response = "ぜったいに"),
+        LessonMaterial(level = 10, prompt = "but", response = "だけど"),
+        LessonMaterial(level = 10, prompt = "next; next time", response = "つぎに"),
+        LessonMaterial(level = 10, prompt = "all together", response = "みんなで"),
+        LessonMaterial(level = 10, prompt = "of course", response = "もちろん"),
         LessonMaterial(
             level = 10,
-            prompt = "open",
-            response = "あける",
-            responseColor = "ru verb"
+            prompt = "now; present",
+            promptColor = "kanji\nnow, this week, this, month, tonight, this semester, this year, today)",
+            response = "今：いま、コン",
+            responseColor = "今（いま）、今（こん）しゅう、今（こん）月、今（こん）ばん、今学（こんがっ）き、今（こ）とし、今日（きょう）"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "be late for; be delayed",
-            response = "おくれる",
-            responseColor = "ru verb, に　particle\nWhy were you late to the party?\nどうして パーティーに おくれましたか。"
+            prompt = "talk, speak, tell; word,language",
+            promptColor = "kanji\njapanese, english, french, to talk",
+            response = "語：かた、ゴ",
+            responseColor = "日本語（にほんご）、えい語（ご）、フランス語（ご）、語（かた）る"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "tell",
-            response = "おしえる",
-            responseColor = "ru verb"
+            prompt = "ahead; previous",
+            promptColor = "kanji\nteacher, previous (semester, week, month), ahead",
+            response = "先：さき、セン",
+            responseColor="先生（せんせい）、先学（センがっ）き、先（せん）しゅう、先月（せんげつ）、先（さき）に"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "close; shut",
-            response = "しめる",
-            responseColor = "ru verb"
+            prompt = "study; learning",
+            promptColor = "kanji\nstudent, college, school, to study",
+            response = "学：まな、ガク ガッ",
+            responseColor="学生（かぐせい）、大学（だいがく）、学（ガッ）こう、学（まな）ぶ"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "inform",
-            response = "知らせる",
-            responseColor = "ru verb\nしらせる"
+            prompt = "live; birth",
+            promptColor = "kanji\nstudent, teacher, to live, to be born, birthday",
+            response = "生：い、う、セイ",
+            responseColor="学生（がくせい）、先生（せんせい）、生（い）きる、生（う）まれる、たん生日（じょうび）"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "turn on",
-            response = "つける",
-            responseColor = "ru verb"
+            prompt = "every",
+            promptColor = "kanji\nevery (day, week, month, year, night)",
+            response = "毎：マイ",
+            responseColor="毎日（まいにち）、毎（まい）しゅう、毎月（まいつき、まいげつ）、毎（まい）ねん、毎（まい）ばん"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "forget",
-            response = "わすれる",
-            responseColor = "ru verb"
+            prompt = "rest",
+            promptColor = "kanji\nto be absent, recess, holiday",
+            response = "休：やす、きょう",
+            responseColor="休（やす）む、休（やす）み、休日（きょうじつ）"
+        ),
+        LessonMaterial(
+            level = 10,
+            prompt = "read",
+            promptColor = "kanji\nto read, reading books",
+            response = "読：よ、どく",
+            responseColor="読（やす）む、読（どく）しょ"
+        ),
+        LessonMaterial(
+            level = 10,
+            prompt = "child",
+            promptColor = "kanji\nMasako, child, electronic dictionary",
+            response = "子：こ、し",
+            responseColor="まさ子（こ）、子（こ）ども、でん子（し）じしょ"
+        ),
+        LessonMaterial(
+            level = 10,
+            prompt = "big",
+            promptColor = "kanji\ncollege, love, big, hard, important, adult",
+            response = "大：おお、だい、たい",
+            responseColor="大学生（だいがくせい）、大（だい）すき、大（おお）きい、大（たい）へん、大（たい）せつ、大人（おとな）"
+        ),
+        LessonMaterial(
+            level = 10,
+            prompt = "come; next",
+            promptColor = "kanji\nnext (week, month, year), to come, will come, won't come",
+            response = "来：く、き、こ、らい",
+            responseColor="来（らい）しゅう、来月（らいげつ）、来（らい）ねん、来（く）る、来（き）ます、来（こ）ない"
+        ),
+        LessonMaterial(
+            level = 10,
+            prompt = "know",
+            promptColor = "kanji\nknow, acquaintance",
+            response = "知：し、ち",
+            responseColor="知（し）る、知人（ちじん）"
+        ),
+        LessonMaterial(
+            level = 10,
+            prompt = "what",
+            promptColor = "kanji\nwhat, what is it, how many people, how many hours",
+            response = "何：なに、なん",
+            responseColor="何（なに）、何（なん）ですか、何人（なんいん）、何（なん）じ"
         ),
         LessonMaterial(
             level = 10,
             prompt = "say",
-            response = "言う",
-            responseColor = "u verb\nいう"
+            promptColor = "kanji\nsay, language, message, word",
+            response = "言：い、げん、ごん、こと",
+            responseColor="言（い）う、言語（げんご）、でん言（ごん）、言（こと）ば"
         ),
         LessonMaterial(
             level = 10,
-            prompt = "finish; end",
-            response = "おわる",
-            responseColor = "u verb, が particle\nThe exam ended.\nしけんが　おわりました。"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "chew",
-            response = "かむ",
-            responseColor = "u verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "do one's best",
-            response = "がんばる",
-            responseColor = "u verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "turn off; erase",
-            response = "けす",
-            responseColor = "u verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "smoke; inhale",
-            response = "すう",
-            responseColor = "u verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "use",
-            response = "つかう",
-            responseColor = "u verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "take",
-            response = "とる",
-            responseColor = "u verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "begin",
-            response = "はじまる",
-            responseColor = "u verb, が particle\nSummer vacation will begin.\nなつ休みが　はじまります。"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "be absent from",
-            response = "休む",
-            responseColor = "u verb, を particle\nやすむ\nYou cannot skip class.\nクラスを　休んでは　いけませんよ。"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "explain",
-            response = "せつめい（を）する",
-            responseColor = "irregular verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "consult",
-            response = "そうだん（を）する",
-            responseColor = "irregular verb, に particle\nWho shall we consult with?\nだれに そうだんしましょうか。"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "practice",
-            response = "れんしゅう（を）する",
-            responseColor = "irregular verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "contact",
-            promptColor = "verb",
-            response = "れんらく（を）する",
-            responseColor = "irregular verb"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "important",
-            response = "大せつ",
-            responseColor = "na adjective\nたいせつ"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "hard",
-            response = "大へん",
-            responseColor = "na adjective\nたいへん"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "no good; bad",
-            response = "だめ",
-            responseColor = "na adjective"
-        ),
-        LessonMaterial(level = 10, prompt = "always", response = "いつも"),
-        LessonMaterial(
-            level = 10,
-            prompt = "without fail; surely",
-            response = "かならず"
-        ),
-        LessonMaterial(level = 10, prompt = "properly", response = "ちゃんと"),
-        LessonMaterial(level = 10, prompt = "yet", response = "まだ"),
-        LessonMaterial(level = 10, prompt = "already", response = "もう"),
-        LessonMaterial(
-            level = 10,
-            prompt = "okay; OK; alright",
-            response = "オーケー"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "Let's study hard.",
-            response = "がんばってべんきょうしましょう。"
-        ),
-        LessonMaterial(
-            level = 10,
-            prompt = "under any circumstance; definitely",
-            response = "ぜったいに"
-        ),
-        LessonMaterial(level = 10, prompt = "but", response = "だけど"),
-        LessonMaterial(level = 10, prompt = "next; next time", response = "つぎに"),
-        LessonMaterial(level = 10, prompt = "all together", response = "みんなで"),
-        LessonMaterial(level = 10, prompt = "of course", response = "もちろん")
+            prompt = "go; conduct; line",
+            promptColor = "kanji\ngo, to carry out, bank, trip, ~ lines",
+            response = "行：い、おこな、こう、ぎょう",
+            responseColor = "行（い）く、行（おこな）う、ぎん行（こう）、りょ行（こう）、ー行（いちぎょう）"
+        )
     )
 }
